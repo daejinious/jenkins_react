82d55c5540aa84cc5e5b7ce06efcd65cf9a66aff
daejinious <size_chi@naver.com>
2023-01-18 11:40:27 +0900

    jenkins

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!55111!
+          Jenkins!!
         </a>
       </header>
     </div>
