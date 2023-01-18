ba44170ecf49d4f62c992ba96eca74901ece0887
daejinious <size_chi@naver.com>
2023-01-18 11:47:26 +0900

    jenkins test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Jenkins!!
+          Jenkins Test!!
         </a>
       </header>
     </div>
