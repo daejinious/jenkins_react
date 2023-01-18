acae5345252e5037f32880fba7197df17cfe0082
daejinious <size_chi@naver.com>
2023-01-18 11:11:12 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!55
+          Learn React!!!55!
         </a>
       </header>
     </div>
